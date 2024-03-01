c03e0184468238fc72a6424a4a58354d9679c1f1
Abdelabset <Abdelbaset.Abidi@studmail.w-hs.de>
2024-03-01 21:54:38 +0100

    commit quizservice and connect to questionservice

--- quizApp/src/main/java/com/projects/quizApp/model/Question.java
+++ quizApp/src/main/java/com/projects/quizApp/model/Question.java
@@ -5,16 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
-@Table(name = "Question")
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Question {
 
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Integer id;
     private String category;
     private String difficultLevel;
